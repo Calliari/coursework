fddc06dddf04de488e57578ed2a81c117b14b1b3
Calliari <ocaliari@spartaglobal.co>
2017-04-04 15:13:04 +0100

    w04d02 AWS intro for VM ubuntu

--- w07d01/starter-code/provision.sh
+++ w07d01/starter-code/provision.sh
@@ -1,3 +1,7 @@
+ # # Destroy vagrant and re-run the Virtual machine 
+ # vagrant destroy --force && vagrant up
+ # ========================================================
+
 #!/bin/bash
 
 # update the source lists
@@ -6,25 +10,64 @@ sudo apt-get update -y
 # Upgrade any package avaiable
 sudo apt-get upgrade -y
 
-# # Package for git 
-# sudo apt-get install git -y
+# Package for git 
+sudo apt-get install git -y
+
+
+
+
+# install ruby dependencies
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev -y
+
+
+# install ruby from rbenv
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+~/.rbenv/bin/rbenv install 2.4.0
+~/.rbenv/bin/rbenv global 2.4.0'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+
+
+ # remove old version of ruby
+ sudo apt-get remove ruby -y
 
  # Package "ruby" should be installed with version "2.4.0"
- # Package "rails" should be installed by "gem"
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-  \curl -sSL https://get.rvm.io | bash -s stable --ruby=2.4.0 --rails 
+~/.rbenv/bin/rbenv install 2.4.0
+~/.rbenv/bin/rbenv global 2.4.0
 
+# rehash rbenv
+~/.rbenv/bin/rbenv rehash
+
+
+# Install gems
+
+ # Package "rails" should be installed by "gem"
+ sudo -H -u vagrant bash -i -c 'gem install rails'
 
  # Package "bundler" should be installed by "gem"
+ sudo -H -u vagrant bash -i -c 'gem install bundler'
 
  # Package "rake" should be installed by "gem"
-
+ sudo -H -u vagrant bash -i -c 'gem install rake'
  
-
 
 
  # # Package "apache2" should be installed
  # # Service "apache2" should be enabled
  # # Service "apache2" should be running
  # # Port "80" should be listening
- # sudo apt-get install apache2 -y
+ sudo apt-get install apache2 -y
+
+
+
+
+
+
+
+
+
+
+
